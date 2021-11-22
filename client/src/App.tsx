2395c897b74f03cc552e795c8a8f74a29e9dce65
lauriahlfors <ahlforslauri@gmail.com>
2021-11-22 13:34:39 +0200

    feat: update App.tsx with router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
-const App = () => {
+// import from react
+import { FC } from 'react';
+// import npm
+import { Routes, Route } from 'react-router-dom';
+// import components
+import Navbar from './components/Navbar';
+// import views
+import Home from './views/Home';
+import Todo from './views/Todo';
+import Info from './views/Info';
+// import stylesheet
+import './scss/index.scss';
+
+const App: FC = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
     </div>
   );
 };
