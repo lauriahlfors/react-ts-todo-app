c27cc19c0efffb177b4e5873cedc2f148a99e4fd
lauriahlfors <ahlforslauri@gmail.com>
2021-11-22 02:39:02 +0200

    feat: update Todo.tsx add functionality

--- client/src/views/Todo.tsx
+++ client/src/views/Todo.tsx
@@ -1,22 +1,81 @@
-// import from react
 import { ChangeEvent, FC, useState } from 'react';
-// import components
-import Input from '../components/Input';
+import { ITodo } from '../interfaces/todo';
+import TodoCard from '../components/TodoCard';
 
 const Todo: FC = () => {
-  const [todo, setTodo] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [deadline, setDeadline] = useState<Date | any>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    event.target.name === 'todo'
-      ? setTodo(event.target.name)
-      : console.log('Yeet');
+    switch (event.target.name) {
+      case 'name':
+        setName(event.target.value);
+        break;
+      case 'description':
+        setDescription(event.target.value);
+        break;
+      case 'deadline':
+        setDeadline(event.target.value);
+    }
+  };
+
+  const resetTodoStates = (): void => {
+    setName('');
+    setDescription('');
+    setDeadline('');
+  };
+
+  const addTodo = (): void => {
+    if (!(name === '' || description === '' || deadline === '')) {
+      let newTodo = {
+        name: name,
+        description: description,
+        deadline: deadline,
+      };
+      setTodos([...todos, newTodo]);
+      resetTodoStates();
+    } else {
+      alert('Both name and deadline must be set');
+    }
   };
 
   return (
     <div className="view view-todo">
       <h1>Todo</h1>
-      <Input name="Todo" type="text" value={todo} onChange={handleChange} />
-      <Input name="Deadline" type="number" />
+      {/* todo name input */}
+      <input
+        type="text"
+        placeholder="Todo name"
+        name="name"
+        value={name}
+        onChange={handleChange}
+      />
+      {/* todo description input */}
+      <input
+        type="text"
+        placeholder="Todo description"
+        name="description"
+        value={description}
+        onChange={handleChange}
+      />
+      {/* todo deadline input */}
+      <input
+        type="date"
+        name="deadline"
+        value={deadline}
+        placeholder="Enter todo deadline"
+        onChange={handleChange}
+      />
+
+      <button onClick={addTodo}>Add task</button>
+
+      <div>
+        {todos.map((todo: ITodo, key: number) => {
+          return <TodoCard key={key} todo={todo} />;
+        })}
+      </div>
     </div>
   );
 };
